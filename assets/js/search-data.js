79dd28ceb2764710a378475f6c3aa0738eaaf630
Minimel <37637069+Minimel@users.noreply.github.com>
2025-08-11 20:41:23 +0000

    Deploying to gh-pages from @ Minimel/Minimel.github.io@16ee6797ac44b2c09cb5854fb011403891cbd0e9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,29 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{
+        },{id: "post-useful-tools-for-a-phd-in-machine-learning",
+        
+          title: "Useful tools for a PhD in Machine Learning",
+        
+        description: "A list of some tools to optimize research and coding",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/Tools/";
+          
+        },
+      },{id: "post-how-to-create-effective-presentations",
+        
+          title: "How to create effective presentations",
+        
+        description: "Lessons from &quot;Storytelling with data&quot; by Cole Nussbaumer",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/DataVisualization/";
+          
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
